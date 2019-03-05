f74b215fa8d06976209b3d6b791d350c0776fe8f
Tucker, Kyle <k.tucker@ufl.edu>
2019-03-04 22:51:14 -0500

    Update test harness

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,6 +7,11 @@ const stylelint = require("stylelint")
 const validStyles = fs.readFileSync("./__tests__/valid.scss", "utf-8")
 const invalidStyles = fs.readFileSync("./__tests__/invalid.scss", "utf-8")
 
+const findWarning = (data, ruleName) => {
+  const warnings = data.results[0].warnings
+  return warnings.find(warning => warning.rule === ruleName)
+}
+
 describe("flags no warnings with valid css", () => {
   let result
 
@@ -28,6 +33,12 @@ describe("flags no warnings with valid css", () => {
       expect(data.results[0].warnings).toHaveLength(0)
     ))
   })
+
+  it("ignores [at-rule-empty-line-before] rule", () => {
+    return result.then(data => {
+      expect(findWarning(data, 'at-rule-empty-line-before')).not.toBeDefined()
+    })
+  })
 })
 
 describe("flags warnings with invalid css", () => {
@@ -40,9 +51,63 @@ describe("flags warnings with invalid css", () => {
     })
   })
 
-  it("does error", () => {
+  it("still does not error", () => {
     return result.then(data => (
-      expect(data.errored).toBeTruthy()
+      expect(data.errored).toBeFalsy()
     ))
+  })
+
+  it("issues exactly 6 warnings", () => {
+    return result.then(data => {
+      expect(data.results[0].warnings).toHaveLength(7)
+    })
+  })
+
+  it("ignores [at-rule-empty-line-before] rule", () => {
+    return result.then(data => {
+      expect(findWarning(data, 'at-rule-empty-line-before')).not.toBeDefined()
+    })
+  })
+
+  it("enforces [at-rule-no-unknown] rule", () => {
+    return result.then(data => {
+      expect(findWarning(data, 'at-rule-no-unknown')).toBeDefined()
+    })
+  })
+
+  it("enforces [rule-empty-line-before] rule", () => {
+    return result.then(data => {
+      expect(findWarning(data, 'rule-empty-line-before')).toBeDefined()
+    })
+  })
+
+  it("enforces [declaration-empty-line-before] rule", () => {
+    return result.then(data => {
+      expect(findWarning(data, 'declaration-empty-line-before')).toBeDefined()
+    })
+  })
+
+  it("enforces [color-hex-case] rule", () => {
+    return result.then(data => {
+      expect(findWarning(data, 'color-hex-case')).toBeDefined()
+    })
+  })
+
+  it("enforces [color-hex-length] rule", () => {
+    return result.then(data => {
+      expect(findWarning(data, 'color-hex-length')).toBeDefined()
+    })
+  })
+
+  it("enforces [block-closing-brace-newline-after] rule", () => {
+    return result.then(data => {
+      expect(findWarning(data, 'block-closing-brace-newline-after')).toBeDefined()
+    })
+  })
+
+  it("enforces [declaration-block-single-line-max-declarations] rule", () => {
+    return result.then(data => {
+      expect(findWarning(data, 'declaration-block-single-line-max-declarations')).toBeDefined()
+    })
   })
 })
